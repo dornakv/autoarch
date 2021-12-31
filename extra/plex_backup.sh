7f1cf2963d3ee393d62f6a7e9fe80439bbcb1f3d
dornakv <vojtechdornak@gmail.com>
2021-12-31 11:50:51 +0100

    Fix paths, exclude metadata and cache from plex backup

--- extra/plex_backup.sh
+++ extra/plex_backup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-source "$(dirname "$0")/../helpers/lib.sh
+source "$(dirname "$0")/../helpers/lib.sh"
 
 plex_data_dir="/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/"
-backup_location="/mnt/kingpin/Backup/plex.tar.gz"
+backup_location='/mnt/kingpin/Backup/plex.tar.gz'
 
-check_root
+# check_root
 
 echo "Creating backup archive into ${backup_location}"
-tar --create --gzip --verbose --file="${backup_location}" ${plex_data_dir}
+tar --create --gzip --verbose --exclude="Cache" --exclude="Metadata" --file="${backup_location}" "${plex_data_dir}"
 echo "Finished. Backup can be found at ${backup_location}"
+
